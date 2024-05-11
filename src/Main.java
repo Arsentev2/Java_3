60efc433a6898903002db548baa00c32fc3f5db8
Arsentev2 <166164116+Arsentev2@users.noreply.github.com>
2024-05-11 22:37:11 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -9,16 +9,15 @@ public class Main {
         int[] array = new int[20];
         Map<Integer, Integer> countMap = new HashMap<>();
 
-        // Заполнение массива случайными числами и подсчет количества вхождений каждого числа
+       
         for (int i = 0; i < array.length; i++) {
             array[i] = rand.nextInt(15) + 1;
             countMap.put(array[i], countMap.getOrDefault(array[i], 0) + 1);
         }
 
-        // Вывод содержимого массива
+
         System.out.println("Содержимое массива: " + Arrays.toString(array));
 
-        // Вывод чисел, которые встречаются больше одного раза
         for (Map.Entry<Integer, Integer> entry : countMap.entrySet()) {
             if (entry.getValue() > 1) {
                 System.out.println("Число '" + entry.getKey() + "' встречается " + entry.getValue() + " раза");
